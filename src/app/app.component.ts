85cca353d053fd8dc5be1662090b0bad8f967600
Carlos Padilla <konnylizamaya@gmail.com>
2021-11-15 16:25:15 -0600

    idiomas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Zoro Ronronoa';
+
+  arreglo: number[] = [1,2,3,4,5,6,7,8,9];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.876;
+
+  salario: number = 2365.09;
+
+  heroe = {
+    nombre: 'Zoro',
+    clave: 'Cazador de Piratas',
+    edad: 24,
+    direccion: {
+      calle: 'Se Perdio',
+      casa: 9
+    }
+  }
+
+  // para usar el pipe 
+  promesa = new Promise<string>((resolve) => {
+    setTimeout(()=> {
+      resolve('llgo el dato');
+    }, 4500);
+  });
+
+  fecha: Date = new Date();
+
+  idioma: string = 'es';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// para agregar la region 
+// en consola ng add @angular/localizer
+
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+
+
 import { AppComponent } from './app.component';
+
+// configurando la region
+registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 @NgModule({
   declarations: [
@@ -10,7 +22,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      // registramos la localidad como un servicio y lo configuramos el idioma
+      // es el idioma por defecto
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
